37b86f3dd42159f22a6fd4cf22c0f93f88b35260
maxsa <maxsa@IMAX>
2024-11-30 20:49:54 +0500

    Добавьте файлы проекта.

---
+++ ProductsList/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ProductsList
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> _products { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            _products = new ObservableCollection<Product>();
+            ProductsList.ItemsSource = _products;
+        }
+
+        private void AddProduct(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+            double price;
+            int quantity;
+
+            Product newProduct;
+
+            if (!double.TryParse(PriceInput.Text, out price) || price <= 0)
+            {
+                MessageBox.Show("Значение цены некорректно.");
+                return;
+            }
+
+            if (QuantityInput.Text == "")
+            {
+                newProduct = new Product(name, price);
+                _products.Add(newProduct);
+
+                NameInput.Clear();
+                PriceInput.Clear();
+                QuantityInput.Clear();
+
+                return;
+
+            }
+            else if (int.TryParse(QuantityInput.Text, out quantity) && quantity > 0)
+            {
+                
+                newProduct = new Product(name, price, quantity);
+                _products.Add(newProduct);
+
+                NameInput.Clear();
+                PriceInput.Clear();
+                QuantityInput.Clear();
+
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Количество товара некорректно.");
+            }
+        }
+
+        private void ShowProductInfo(object sender, RoutedEventArgs e)
+        {
+           if (ProductsList.SelectedItem is Product selectedItem)
+            {
+                selectedItem.Deconstruct(out string name, out double price, out int quantity);
+                MessageBox.Show($"Название: {name} \nЦена: {price} \nКоличество: {quantity} шт.", "Информация о товаре");
+            }
+            
+        }
+    }
+}
---
+++ ProductsList/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsList
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) 
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            Quantity = 1;
+        }
+
+        public void Deconstruct(out string name, out double price, out int quantity)
+        {
+            name = Name;
+            price = Price;
+            quantity = Quantity;
+        }
+    }
+}
